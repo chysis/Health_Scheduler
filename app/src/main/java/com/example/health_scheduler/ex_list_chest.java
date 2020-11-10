5ab57590ffff2139ed546ff531b1cd997e62be2b
Jun <jjy0224y@gmail.com>
2020-11-10 18:00:45 +0900

    운동리스트추가(팔,어깨,가슴)

---
+++ app/src/main/java/com/example/health_scheduler/ex_list_chest.java
@@ -0,0 +1,14 @@
+package com.example.health_scheduler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ex_list_chest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ex_list_chest);
+    }
+}
