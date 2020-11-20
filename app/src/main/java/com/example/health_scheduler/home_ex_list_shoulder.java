fd9a8450a97a3ba93ccaacc49d544ad1b531914c
Jun <jjy0224y@gmail.com>
2020-11-21 06:26:19 +0900

    홈트-팔,어깨,가슴-xml파일

---
+++ app/src/main/java/com/example/health_scheduler/home_ex_list_shoulder.java
@@ -0,0 +1,14 @@
+package com.example.health_scheduler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class home_ex_list_shoulder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_ex_list_shoulder);
+    }
+}
