7e69689bad7c01793ef927604963c40cbe7ae0ed
chysis <chysiss@naver.com>
2020-11-21 01:32:38 +0900

    캘린더 부분 구현, 리스트 화면 틀 제작

--- app/src/main/java/com/example/health_scheduler/added_list.java
+++ app/src/main/java/com/example/health_scheduler/added_list.java
@@ -1,7 +1,12 @@
 package com.example.health_scheduler;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +18,11 @@ import com.github.mikephil.charting.data.PieEntry;
 import java.util.ArrayList;
 
 public class added_list extends AppCompatActivity {
+    ListView listView;
+    public static Context context_main;
+    public int var;  // 다른 액티비티에서 접근할 변수
+    final ArrayList<String> items = new ArrayList<String>();
+    final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_single_choice, items);
 
     PieChart pieChart;
     int[] colorArr = new int[] {Color.RED,Color.GREEN,Color.BLUE,Color.LTGRAY,Color.CYAN};
@@ -21,6 +31,8 @@ public class added_list extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_added_list);
+
+        context_main=this;
 
         pieChart = (PieChart)findViewById(R.id.piechart);
 
@@ -32,6 +44,38 @@ public class added_list extends AppCompatActivity {
         pieChart.setData(pieData);
         pieChart.invalidate();
 
+        final ListView listview = (ListView) findViewById(R.id.exListView);
+        listview.setAdapter(adapter);
+
+        Button addButton = (Button)findViewById(R.id.btnAdd);
+        addButton.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+                int count;
+                count=adapter.getCount();
+
+                items.add("LIST" + Integer.toString(count+1));
+
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        Button deleteButton = (Button)findViewById(R.id.btnDelete);
+        deleteButton.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+                int count, checked;
+                count=adapter.getCount();
+
+                if(count>0){
+                    checked=listview.getCheckedItemPosition();
+
+                    if(checked>-1&&checked<count){
+                        items.remove(checked);
+                        listview.clearChoices();
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+            }
+        });
     }
 
     private ArrayList<PieEntry> data1() {
@@ -45,5 +89,7 @@ public class added_list extends AppCompatActivity {
 
         return datavalue;
     }
+
+
 
 }
--- app/src/main/java/com/example/health_scheduler/ex_dumbbell_curl.java
+++ app/src/main/java/com/example/health_scheduler/ex_dumbbell_curl.java
@@ -1,16 +1,24 @@
 package com.example.health_scheduler;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
+
+import java.util.ArrayList;
 
 public class ex_dumbbell_curl extends YouTubeBaseActivity {
     YouTubePlayerView playerView;
@@ -30,13 +38,48 @@ public class ex_dumbbell_curl extends YouTubeBaseActivity {
         initPlayer();
 
         Button button = findViewById(R.id.playDumbCurlVideo);
+        final Button button2 = findViewById(R.id.btnDumbCurlAdd);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 playVideo();
             }
         });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dlg = new AlertDialog.Builder(ex_dumbbell_curl.this);
+                dlg.setTitle("모드 선택");
+                final String[] versionArray = new String[] {"근지구력 (15회 3세트)", "근력 (6회 5세트)", "사용자 설정"};
+                //dlg.setIcon(R.drawable.)
+
+                dlg.setItems(versionArray, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //button2.setText(versionArray[which]);
+                        Intent intent = new Intent(getApplicationContext(), added_list.class);
+                        intent.putExtra("덤벨 컬", versionArray[which]);
+                        setResult(RESULT_OK, intent);
+                        Toast toast = Toast.makeText(ex_dumbbell_curl.this, versionArray[which], Toast.LENGTH_LONG);
+                        toast.show();
+                        ListView listView = ((added_list)added_list.context_main).listView;
+                        Adapter adapter = listView.getAdapter();
+                        ArrayList<String> arrayList = ((added_list)added_list.context_main).items;
+
+                    }
+                });
+                dlg.setPositiveButton("닫기", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(ex_dumbbell_curl.this, "취소", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                dlg.show();
+            }
+        });
     }
+
     public void initPlayer(){
         playerView = findViewById(R.id.dumbCurlView);
         playerView.initialize(API_KEY, new YouTubePlayer.OnInitializedListener() {
