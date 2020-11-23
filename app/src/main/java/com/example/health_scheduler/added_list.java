119854afe24e0181c7d74d3c398bd670141c48f2
Jun <jjy0224y@gmail.com>
2020-11-23 21:29:55 +0900

    일단 추가버튼 눌렀을때 리스트에 추가되는 기능

--- app/src/main/java/com/example/health_scheduler/added_list.java
+++ app/src/main/java/com/example/health_scheduler/added_list.java
@@ -1,13 +1,18 @@
 package com.example.health_scheduler;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
@@ -17,7 +22,6 @@ import com.github.mikephil.charting.data.PieEntry;
 import java.util.ArrayList;
 
 public class added_list extends AppCompatActivity {
-    ListView listView;
 
     PieChart pieChart;
     int[] colorArr = new int[] {Color.RED,Color.GREEN,Color.BLUE,Color.LTGRAY,Color.CYAN};
@@ -37,30 +41,89 @@ public class added_list extends AppCompatActivity {
         pieChart.setData(pieData);
         pieChart.invalidate();
 
-        final ListView listview = (ListView) findViewById(R.id.exListView);
 
-        Button addButton = (Button)findViewById(R.id.btnAdd);
-        addButton.setOnClickListener(new Button.OnClickListener(){
-            public void onClick(View v){
+        ListView exListView ;
+        final ListViewAdapter adapter;
 
-            }
-        });
+        // Adapter 생성
+        adapter = new ListViewAdapter() ;
 
-        Button deleteButton = (Button)findViewById(R.id.btnDelete);
-        deleteButton.setOnClickListener(new Button.OnClickListener(){
-            public void onClick(View v){
+        // 리스트뷰 참조 및 Adapter달기
+        exListView = (ListView) findViewById(R.id.exListView);
+        exListView.setAdapter(adapter);
 
-            }
-        });
 
-        Button button2 = findViewById(R.id.btnStart);
-        button2.setOnClickListener(new View.OnClickListener(){
+
+
+        final Button add = (Button) findViewById(R.id.btnAdd);
+
+        add.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                Intent intent = new Intent(getApplicationContext(), pause_display.class);
-                startActivity(intent);
+            public void onClick(View v) {
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(added_list.this);
+                alertDialog.setTitle("운동장소 선택");
+                String[] items = {"헬스장","홈트"};
+                int checkedItem = 1;
+                alertDialog.setSingleChoiceItems(items, checkedItem, new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            // 헬스장 눌렀을때
+                            case 0:
+                                AlertDialog.Builder alertDialog1 = new AlertDialog.Builder(added_list.this);
+                                alertDialog1.setTitle("운동선택");
+                                String[] items = {"덤벨컬","해머컬","1","2","3","4","5","6","7","8","9","10","11","12","13"};
+                                int checkedItem = 1;
+                                alertDialog1.setSingleChoiceItems(items, checkedItem, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        switch (which) {
+                                            case 0:
+                                                int count;
+                                                count = adapter.getCount();
+
+                                                // 아이템 추가.
+                                                adapter.addItem(ContextCompat.getDrawable(getApplicationContext(), R.drawable.dumbbell_curl),
+                                                        "덤벨컬");
+
+                                                // listview 갱신
+                                                adapter.notifyDataSetChanged();
+                                                break;
+                                            case 1:
+
+                                                count = adapter.getCount();
+
+                                                // 아이템 추가.
+                                                adapter.addItem(ContextCompat.getDrawable(getApplicationContext(), R.drawable.hammer_curl),
+                                                        "해머컬");
+
+                                                // listview 갱신
+                                                adapter.notifyDataSetChanged();
+                                                break;
+
+                                        }
+                                    }
+                                });
+                                AlertDialog alert = alertDialog1.create();
+                                alert.setCanceledOnTouchOutside(false);
+                                alert.show();
+                                break;
+
+                            // 홈트 눌렀을때
+                            case 1:
+
+                                break;
+
+                        }
+                    }
+                });
+                AlertDialog alert = alertDialog.create();
+                alert.setCanceledOnTouchOutside(false);
+                alert.show();
             }
         });
+        // 리스트뷰 아이템 추가 끝
     }
 
     private ArrayList<PieEntry> data1() {
